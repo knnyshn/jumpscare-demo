91bbd9dc8454fd7521d86e77035af6e4b7b82ff7
Kenny Shin <knnyshn@gmail.com>
2023-08-27 20:04:22 -0400

    Updates

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,24 @@
 import React from "react"
 
 const Image = ({ id }) => {
+  const handleClick = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = Math.round(e.clientX - rect.left);
+    const y = Math.round(e.clientY - rect.top);
+    console.log(`x: ${x}, y: ${y}`);
+  }
+
   return (
     <>
-    <img src={`./assets/${id}.jpeg`} alt="" />
+      <img
+        className="no-drag"
+        src={`./assets/${id}.jpeg`}
+        alt="dammit"
+        onClick={handleClick}
+      />
     </>
   )
 }
+
 
 export default Image;
